8979a675a8c8f280b4811d48a3760137e34af82b
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-07-25 12:43:03 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const fn = function () {
-  console.log('Hello World');
-};
-fn()
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 4000;
+
+//MiddleWares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
